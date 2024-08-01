85d0a9d34f3d05fb9f248704d5e5eb3383ab5fe9
gunjanD99 <gunjandhoke5@gmail.com>
2024-08-01 19:08:25 +0530

    my version-5

--- src/components/Sociallinks.js
+++ src/components/Sociallinks.js
@@ -2,7 +2,6 @@ import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { GrMail } from "react-icons/gr";
-import { IoMdMail } from "react-icons/io";
 
 function Sociallinks() {
   const links = [
